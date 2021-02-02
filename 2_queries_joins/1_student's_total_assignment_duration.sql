331f118b0120654e6b9190a9933c991f47374bfd
DrMustafaH <mustafaharith@gmail.com>
2021-02-01 16:35:37 -0800

    Implement join querries

---
+++ 2_queries_joins/1_student's_total_assignment_duration.sql
@@ -0,0 +1,15 @@
+SELECT 
+SUM(assignment_submissions.duration) as total_duration
+FROM
+assignment_submissions , students
+WHERE
+assignment_submissions.student_id = students.id AND students.name = 'Ibrahim Schimmel';
+
+
+
+-- answer in compass is :
+
+-- SELECT sum(assignment_submissions.duration) as total_duration
+-- FROM assignment_submissions
+-- JOIN students ON students.id = student_id
+-- WHERE students.name = 'Ibrahim Schimmel';
